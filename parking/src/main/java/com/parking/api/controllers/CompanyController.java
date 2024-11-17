c1c0834388e4313d00148e18a0493437e88d2833
Paulo Almeida <pauloalmeida.jrr@gmail.com>
2024-11-17 02:59:17 -0300

    add company controller

---
+++ parking/src/main/java/com/parking/api/controllers/CompanyController.java
@@ -0,0 +1,28 @@
+package com.parking.api.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.parking.domain.entities.Company;
+import com.parking.domain.services.CompanyService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/companies")
+public class CompanyController {
+
+  private final CompanyService companyService;
+
+  @PostMapping
+  public ResponseEntity<Void> createCompany(@Valid @RequestBody Company company) {
+    companyService.createCompany(company);
+    return ResponseEntity.status(HttpStatus.CREATED).build();
+  }
+}
