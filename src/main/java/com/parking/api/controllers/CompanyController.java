6f957a5db5123f20ead58a78896d50e6e48e284e
Paulo Almeida <pauloalmeida.jrr@gmail.com>
2025-01-05 18:58:23 -0300

    refactor function getCompany

--- src/main/java/com/parking/api/controllers/CompanyController.java
+++ src/main/java/com/parking/api/controllers/CompanyController.java
@@ -1,15 +1,22 @@
 package com.parking.api.controllers;
 
-import com.parking.domain.entities.Company;
-import com.parking.domain.repositories.CompanyRepository;
-import com.parking.domain.services.CompanyService;
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
+import java.util.Optional;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
+import com.parking.api.responses.GetCompanyResponse;
+import com.parking.domain.entities.Company;
+import com.parking.domain.services.CompanyService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,7 +24,6 @@ import java.util.Optional;
 public class CompanyController {
 
     private final CompanyService companyService;
-    private final CompanyRepository companyRepository;
 
     @PostMapping("/create")
     public ResponseEntity<Void> createCompany(@Valid @RequestBody Company company) {
@@ -26,12 +32,20 @@ public class CompanyController {
     }
 
     @GetMapping()
-    public ResponseEntity<Optional<Company>> findCompany(@Valid @RequestParam String cnpj) {
-        Optional<Company> company = companyRepository.findByCnpj(cnpj);
+    public ResponseEntity<GetCompanyResponse> findCompany(@Valid @RequestParam String cnpj) {
+        Optional<Company> company = companyService.findCompanyByCnpj(cnpj);
 
-        if (company.isEmpty()) {
-            throw new RuntimeException("Company not found");
-        }
-        return ResponseEntity.ok(company);
+        GetCompanyResponse getCompanyResponse = new GetCompanyResponse(
+                company.get().getId(),
+                company.get().getName(),
+                company.get().getCnpj(),
+                company.get().getAddress(),
+                company.get().getPhone(),
+                company.get().getMotorcycleParkingSpace(),
+                company.get().getCarParkingSpace(),
+                company.get().getCreatedAt(),
+                company.get().getUpdatedAt());
+
+        return ResponseEntity.ok(getCompanyResponse);
     }
 }
--- src/main/java/com/parking/domain/services/CompanyService.java
+++ src/main/java/com/parking/domain/services/CompanyService.java
@@ -1,10 +1,14 @@
 package com.parking.domain.services;
 
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
 import com.parking.api.exceptions.CnpjAlreadyExistsException;
 import com.parking.domain.entities.Company;
 import com.parking.domain.repositories.CompanyRepository;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
@@ -14,12 +18,21 @@ public class CompanyService {
     private final AuthService authService;
 
     public void createCompany(Company company) {
-
         if (companyRepository.findByCnpj(company.getCnpj()).isPresent()) {
             throw new CnpjAlreadyExistsException("Company already exists.");
         }
 
         company.setPassword(authService.passwordToHash(company.getPassword()));
         companyRepository.save(company);
+    }
+
+    public Optional<Company> findCompanyByCnpj(String cnpj) {
+        Optional<Company> company = companyRepository.findByCnpj(cnpj);
+
+        if (company.isEmpty()) {
+            throw new RuntimeException("Company not found");
+        }
+
+        return company;
     }
 }
