8b2df8479367747c65a072d2267d7b4bc4baaef8
Paulo Almeida <pauloalmeida.jrr@gmail.com>
2025-01-08 21:40:58 -0300

    add updateCompany request

--- src/main/java/com/parking/domain/services/CompanyService.java
+++ src/main/java/com/parking/domain/services/CompanyService.java
@@ -1,9 +1,13 @@
 package com.parking.domain.services;
 
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+
 import org.springframework.stereotype.Service;
 
 import com.parking.api.exceptions.CnpjAlreadyExistsException;
 import com.parking.api.exceptions.CompanyNotFoundException;
+import com.parking.api.responses.UpdateCompanyRequest;
 import com.parking.domain.entities.Company;
 import com.parking.domain.repositories.CompanyRepository;
 
@@ -29,5 +33,30 @@ public class CompanyService {
         return companyRepository.findByCnpj(cnpj)
                 .orElseThrow(() -> new CompanyNotFoundException("Company not found"));
 
+    }
+
+    public Company updateCompany(Long id, UpdateCompanyRequest request) {
+        Company company = companyRepository.findById(id)
+                .orElseThrow(() -> new CompanyNotFoundException("Company not found"));
+
+        if (request.name() != null && !request.name().isBlank()) {
+            company.setName(request.name());
+        }
+        if (request.address() != null && !request.address().isBlank()) {
+            company.setAddress(request.address());
+        }
+        if (request.phone() != null && !request.phone().isBlank()) {
+            company.setPhone(request.phone());
+        }
+        if (request.motorcycleParkingSpace() != null && request.motorcycleParkingSpace() >= 0) {
+            company.setMotorcycleParkingSpace(request.motorcycleParkingSpace());
+        }
+        if (request.carParkingSpace() != null && request.carParkingSpace() >= 0) {
+            company.setCarParkingSpace(request.carParkingSpace());
+        }
+
+        company.setUpdatedAt(OffsetDateTime.now(ZoneOffset.UTC));
+
+        return companyRepository.save(company);
     }
 }
