d1e8b4dee9dad909111ea97f20388ffa94b63970
Paulo Almeida <pauloalmeida.jrr@gmail.com>
2025-01-08 22:09:36 -0300

    refactor findCompany to path param

--- src/main/java/com/parking/api/controllers/CompanyController.java
+++ src/main/java/com/parking/api/controllers/CompanyController.java
@@ -33,8 +33,8 @@ public class CompanyController {
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-    @GetMapping()
-    public ResponseEntity<CompanyResponse> findCompany(@Valid @RequestParam String cnpj) {
+    @GetMapping("/{cnpj}")
+    public ResponseEntity<CompanyResponse> findCompany(@Valid @PathVariable String cnpj) {
         Company company = companyService.findCompanyByCnpj(cnpj);
 
         CompanyResponse getCompanyResponse = new CompanyResponse(
